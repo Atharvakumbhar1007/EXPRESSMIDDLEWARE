2eb5341e12b96343516fb4cf72aba96328136cf4
Atharva <atharvakumbhar631@gmail.com>
2026-06-23 15:16:31 +0530

    raw

--- src/Routes/product.ts
+++ src/Routes/product.ts
@@ -4,3 +4,15 @@ export interface Product{
     category:string;
     instock:boolean;
 }
+import express from "express";
+import { body } from "express-validator";
+
+// VALIDATION - Checks data is correct or not 
+const app = express();
+app.use(express.json());
+app.post("/product", [
+    body("name")
+    .trim()
+    .notEmpty().withMessage("Product name is required")
+    .isLength({min:4, max:150}).withMessage(("Name too Short"))
+])
